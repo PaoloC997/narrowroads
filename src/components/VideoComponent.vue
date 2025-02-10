f8cf4119dfda78d3f6947f8bd24eb4ac3b0fb082
Paolo <paoloc9997@gmail.com>
2025-02-11 00:11:23 +0100

    add video and logos

--- src/components/VideoComponent.vue
+++ src/components/VideoComponent.vue
@@ -2,14 +2,26 @@
     <div class="parent">
         <div class="bg"></div>
         <img src="../assets/logo4.webp" class="islands" />
-        <video src="../assets/Portada_01.mp4" autoplay playsinline muted></video>
-        <div class="text-container">
-            <p>"Do not seek to follow in the footsteps of the wise;
-                seek what they sought."- Matsuo Basho
+        <video src="../assets/Portada_01.mp4" autoplay playsinline muted loop></video>
+        <div class="text-container grotesk">
+            <p class="text">"Do not seek to follow in the footsteps of the wise;
+                seek what they sought." <br><span class="italic"> <em>- Matsuo Basho</em></span>
             </p>
         </div>
-     
         <img src="../assets/logobig.webp" class="logo-big" />
+        <div class="logos-container">
+            <div class="flex-left">
+                <img class="logo-sm" src="../assets/ah2.webp"/>
+                <img class="logo-sm2"  src="../assets/generalitat.webp"/>
+                <img class="logo-sm"  src="../assets/logo2.webp"/>
+                <img class="logo-sm3"  src="../assets/logo5.webp"/>
+            </div>
+            <div class="flex-right">
+                texty text
+            </div>
+
+        </div>
+
     </div>
 </template>
 
@@ -24,10 +36,14 @@ export default {
     position: relative;
     height: 100vh;
     width: 100vw;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    z-index:1;
 }
 
 .bg {
-    background-color: lightblue;
+    background-color: blue;
     height: 100vh;
     z-index: 0;
 }
@@ -39,18 +55,17 @@ video {
     z-index: 100;
     top: 0;
     position: absolute;
-    opacity: 0.8;
+    filter: opacity(0.9);
 }
 
 .logo-big {
     position: absolute;
     top: 30%;
     bottom: 50%;
-    margin: auto;
-    left: 15%;
-    right: 15%;
     z-index: 101;
-    transform: scale(0.9);
+    
+    justify-self: center;
+    align-self: center;
 }
 
 .islands{
@@ -64,15 +79,65 @@ video {
 
 .text-container{
     position: absolute;
-    top: 5%;
+    top: 60%;
     margin: auto;
     left: 15%;
     right: 15%;
     z-index: 101;
-    width:30vw;
+    width:45vw;
     color:white;
-    
+    text-align: center;
+    line-height: 23px;
+    letter-spacing: 1.5px;
+    font-weight:300;
 }
 
+.text{
+    font-size: 1.7rem;
+}
+
+.italic{
+    font-size: 1.2rem;
+}
+
+.logos-container{
+    width:100vw;
+    height:10vh;
+    display:flex;
+    flex-direction: row;
+    position:absolute;
+    bottom:2%;
+    z-index: 105;
+    margin-left: 2rem;
+    margin-right: 2rem;
+}
+
+.flex-left{
+    display: flex;
+    flex:1;
+    flex-direction: row;
+    justify-content:space-between;
+    align-items: center;
+}
+
+.flex-right{
+    display: flex;
+  
+    flex:1;
+    flex-direction: row;
+}
+
+.logo-sm{
+    height:5vh;
+}
+
+.logo-sm2{
+    height:5vh;
+}
+
+.logo-sm3{
+    height:8vh;
+    margin-bottom: 1.5rem;
+}
 
 </style>
