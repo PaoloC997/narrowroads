15dfbe1d0c6a53e9340e712fe35f3469db058b63
Paolo <paoloc9997@gmail.com>
2025-02-11 22:43:35 +0100

    add src to gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
+     <router-view></router-view>
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/projects">Projects</router-link>
     </nav>
-    <router-view></router-view>
   </div>
 </template>
 
