e4ea194cd2af0d2c98a4e76fa9ae0a88f4e10199
Paolo Carretero <sandrapaolaviacava@Air-de-Paolo.home>
2025-02-13 23:47:41 +0100

    add description component

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <VideoComponent />
     <IntroSection />
+    <ProjectDescription/>
   </div>
   
 </template>
@@ -9,11 +10,13 @@
 <script>
 import VideoComponent from './VideoComponent.vue';
 import IntroSection from './IntroSection.vue';
+import ProjectDescription from './ProjectDescription.vue';
 export default {
   name: 'Home',
   components: {
     VideoComponent,
-    IntroSection
+    IntroSection,
+    ProjectDescription
   }
 };
 </script>
---
+++ src/components/Home/ProjectDescription.vue
@@ -0,0 +1,122 @@
+<template>
+    <div>
+        <img class="d-image" src="../../../public/assets/d-image.webp" />
+    </div>
+    <div class="d-container">
+        <h2 class="d-title">THE PATH OF SOUND</h2>
+        <div class="d-flex">
+            <div class="d-flex-left">
+                <p class="grotesk"> When I enter the room, the first image that greets me is Ginebra Raventós, Ɇ₥łⱠłØ ₥₳ⱤӾ, Joan
+                    Lavandeira and
+                    Edgardo Gómez sitting cross-legged on a tatami around a table. Three screens surround them,
+                    displaying
+                    black-and-white moving images. In front of them, an array of devices and cables, each having its
+                    place and
+                    function.
+                    The ceremony of sound has begun.
+                    In Japan, they follow the concept of Do, derived from Tao: the path, the protocol, the instructions
+                    to
+                    master
+                    a technique.
+                    Acoustic Heritage Collectives takes the path of sound
+                    and image, pressing the right keys with studied precision
+                    in order to introduce us to the journey that led them to follow the steps of another path: the one
+                    taken by
+                    Matsuo Basho to reconnect with the most beautiful haikus and the places where they were originally
+                    written.
+                    The initial cultural shock, the contrast between expectations and what was found upon landing in the
+                    city
+                    of Tokyo and its impact on the senses: the people, the signs, the advertisements, the jingles, will
+                    be
+                    abruptly interrupted by the encounter with the lurking monster of the earthquake.
+                    For Japan, in its fragility as an island, is always at the mercy of natural disasters. Tsunami is a
+                    Japanese
+                    word. Hiroshima and Nagasaki are two places that, despite their millennia-old etymology, evoke
+                    danger. And
+                    Godzilla is a Japanese monster, pop mythology at the dawn of the new gods of mass media.
+                    But there is a journey within the journey: the impossibility of translation. Translating the haikus
+                    from
+                    Japanese, a language where context is crucial, in which each kanji can visually describe multiple
+                    concepts,
+                    involves the death and rebirth of the poem, transforming it into something similar, but almost.
+                    Octavio Paz
+                    and others embarked on this adventure, deconstructing the haikus to reassemble them into something
+                    that
+                    could say the same.
+                    The essence of the haiku is itself elusive: it only makes sense in its context. In the case of
+                    haikus, the
+                    poet and their feelings disappear in favor of the surroundings. The poetic voice emerges from the
+                    landscape
+                    upon which the poet lays their eyes, or from the relationship between elements and time (whether
+                    atmospheric
+                    or sequential). 
+                </p>
+            </div>
+            <div class="d-flex-right">
+                <p>Narrow Backroads dissolves authorship into the collective and leaves the viewer to complete the
+                    journey with
+                    the sensations awakened.</p>
+                <p class="grotesk">Similarly, just as in haikus the poet's identity fades in favor of the landscape, being collective
+                    compositions, it remains open for the reader to interpret and complete its meaning, Narrow Backroads
+                    dissolves authorship into the collective and leaves the viewer to complete the journey with the
+                    sensations
+                    awakened.
+                    Acoustic Heritage Collective impacts us visually in an immersive environment, guiding us into
+                    silence and
+                    stillness, immersing us in the altered state of consciousness that comes with traveling to an
+                    unknown land,
+                    attempting to tell us their memory of the journey, a memory composed of many voices. With the same
+                    kamikaze
+                    intent as one who tries to translate something untranslatable, something that can only be lived
+                    through
+                    experience. Memory, translation, the trap.
+                    When all signs point to the fact that we cannot understand it all, the best thing is to surrender to
+                    feeling. In their attempt, let us be carried away by the senses.”</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProjectDescription',
+};
+</script>
+<style>
+.d-image {
+    height: 100vh;
+    width: 100vw;
+}
+
+.d-container {
+    margin: 5rem;
+}
+
+.i-title {
+    margin-bottom: 2rem;
+    color: gray;
+    font-size: 3rem;
+}
+.d-flex {
+    display: flex;
+    flex-direction: row;
+    flex: 1;
+    letter-spacing: 2px;
+}
+
+.d-flex-left {
+    flex: 1;
+    letter-spacing: 2px;
+}
+
+.d-flex-left p , .d-flex-right p {
+    line-height: 2.4rem;
+    font-weight: 300;
+    font-size: 1.5rem;
+}
+
+.d-flex-right {
+    flex: 1;
+    letter-spacing: 2px;
+}
+</style>
